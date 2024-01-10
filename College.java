301e00259c0d73dbe4cf9a01ba756c3d912d4f35
mr-fahad-666 <156161928+mr-fahad-666@users.noreply.github.com>
2024-01-10 12:43:13 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,49 @@
+class Student {
+    int rollNo;
+    String sName;
+    String scourse;
+    long hallTicket;
+    String address;
+
+    Student(int rollNo, String sName, String scourse, long hallTicket, String address) {
+        this.rollNo = rollNo;
+        this.sName = sName;
+        this.scourse = scourse;
+        this.hallTicket = hallTicket;
+        this.address = address;
+    }
+
+}
+
+class College {
+    void m1() {
+        Student s = new Student(101, "SHRISHA", "fULL sTACH", 79998989L, "aMEERTPET");
+        System.out.println("RollNo " + s.rollNo);
+        System.out.println("Name " + s.sName);
+        System.out.println("Course " + s.scourse);
+        System.out.println("HallTicketNO. " + s.hallTicket);
+        System.out.println("Address " + s.address);
+
+    }
+
+    Student m2() {
+        Student s = new Student(102, "FAHAD", "fULL sTACH", 8686020767L, "malakpet");
+        System.out.println();
+        System.out.println("RollNo " + s.rollNo);
+        System.out.println("Name " + s.sName);
+        System.out.println("Course " + s.scourse);
+        System.out.println("HallTicketNO. " + s.hallTicket);
+        System.out.println("Address " + s.address);
+
+
+        return s;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        College c = new College();
+        c.m1();
+        c.m2();
+    }
+}
